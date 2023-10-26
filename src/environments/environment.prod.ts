0aeb570ba8ed33a2d460678fb572f9d61db0560e
FGalleguillos <feli.galleguillos@duocuc.cl>
2023-10-26 02:02:14 -0300

    avance auth y guards

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyDkaFEzzDoheUioBYFjco4R-ZdN-GyzACE",
+    authDomain: "product-admin-app-2ab34.firebaseapp.com",
+    projectId: "product-admin-app-2ab34",
+    storageBucket: "product-admin-app-2ab34.appspot.com",
+    messagingSenderId: "615455099830",
+    appId: "1:615455099830:web:497e01601fcddd7a36cf17"
+  }
 };
